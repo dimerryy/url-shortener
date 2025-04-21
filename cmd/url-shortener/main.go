2b6e4402c9e0c8732eb64c0eb7b1a967627ad46b
dibadull <dinmukhammed.ibadulla@nu.edu.kz>
2025-04-21 22:40:25 +0500

    feat: set up slog for structured logging

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
-	"example/Go/projects/url-shortener/internal/config"
 	"fmt"
+	"log/slog"
+	"os"
+	"url-shortener/internal/config"
 )
 
 func main() {
 	cfg := config.LoadConfig()
 	fmt.Println(cfg)
+
+	log := slog.New(
+		slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+	)
+
+	log.Info("starting url-shortener")
 }
